7d7d68baa41377cfcf11af5dbe2df057e33f36cd
MustafaTv <samychattali@me.com>
2024-09-19 16:27:45 -0400

    Added custom validator for confirm password

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -3,6 +3,7 @@
 const pool = require("../db/pool")
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 async function renderIndex(req, res) {
     res.render("index");
@@ -17,17 +18,31 @@ async function getSignUp(req, res) {
 }
 
 async function postSignUp(req, res, next) {
+    await check("password", "Password must be at least 5 characters long")
+        .isLength({ min: 5 })
+        .run(req);
+
+    await check("confirmPassword", "Passwords do not match")
+        .custom((value, { req }) => value === req.body.password)
+        .run(req);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render("sign-up-form", {
+            errors: errors.array(),
+        });
+    }
+
     try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  
-      await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-        req.body.username,
-        hashedPassword,
-      ]);
-  
-      res.redirect("/log-in");
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+            req.body.username,
+            hashedPassword,
+        ]);
+
+        res.redirect("/log-in");
     } catch (err) {
-      return next(err);
+        return next(err);
     }
 }
 
@@ -46,7 +61,8 @@ async function postLogin(req, res, next) {
 
 async function logOut(req, res) {
     req.logout((err) => {
-        if (err) { return next(err); }
+        if (err) { return next(err);
+        }
         res.redirect('/log-in');
     });
 }
